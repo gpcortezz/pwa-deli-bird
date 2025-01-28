084148c21ec9d3f207d836e96a8992fe3c5e627e
gpcortezz <giovannipliego1006@gmail.com>
2025-01-28 14:30:52 -0800

    deploy pwa

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+import withPWA from 'next-pwa';
+
+const nextConfig = withPWA({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+})({
+  reactStrictMode: true,
+});
 
 export default nextConfig;
